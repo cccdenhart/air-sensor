0ec07cfae27b1150a93984206d8f040d9a95da5a
Charlie Denhart <cccdenhart@icloud.com>
2024-09-19 08:10:16 -0700

    Add date filtering

--- Home.py
+++ Home.py
@@ -1,27 +1,59 @@
-import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
 import pandas as pd
 import streamlit as st
 from supabase import Client, create_client
 
 
 # Initialize connection to Supabase
-def get_data():
+def get_data(start_date: datetime, end_date: datetime):
     url: str = st.secrets["SUPABASE_POST_URL"]
     key: str = st.secrets["SUPABASE_KEY"]
     supabase: Client = create_client(url, key)
 
-    response = supabase.table('sensor').select('*').order("created_at", desc=True).limit(1000).execute()
+    response = (
+        supabase.table("sensor")
+        .select("*")
+        .gte("created_at", start_date)
+        .lte("created_at", end_date)
+        .order("created_at", desc=True)
+        .limit(1000)
+        .execute()
+    )
 
     df = pd.DataFrame(response.data)
 
-    df["created_at"] = df["created_at"].apply(pd.to_datetime).dt.tz_convert('US/Pacific')
+    df["created_at"] = (
+        df["created_at"].apply(pd.to_datetime).dt.tz_convert("US/Pacific")
+    )
 
     return df
 
 
 st.title("Home Air Quality Report")
 
-df = get_data()
+st.subheader("Filter Data")
+default_start = datetime.now() - timedelta(days=3)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    start_date = st.date_input("Start Date", value=default_start)
+with col2:
+    end_date = st.date_input("End Date", value=datetime.now())
+with col3:
+    location = st.selectbox("Location", options=["Kitchen", "Bedroom 3"])
+
+if start_date > end_date:
+    st.error("Start date must be before the end date.")
+    st.stop()
+
+if start_date > datetime.now().date() or end_date > datetime.now().date():
+    st.error("You cannot select a time in the future.")
+    st.stop()
+
+st.subheader("Data")
+
+df = get_data(start_date, end_date)
 
 st.subheader("CO2 PPM")
 st.line_chart(df, x="created_at", y=["co2_ppm"])
@@ -33,16 +65,17 @@ st.subheader("Air Quality")
 
 st.write("**Standard Measurements**")
 standard_metrics = [c for c in df.columns if "standard" in c]
-standard_metrics = sorted(standard_metrics, key=lambda x: int(x.split()[0][2:])) 
+standard_metrics = sorted(standard_metrics, key=lambda x: int(x.split()[0][2:]))
 st.line_chart(df, x="created_at", y=standard_metrics)
 
 st.write("**Environment Measurements**")
 env_metrics = [c for c in df.columns if "env" in c]
-env_metrics = sorted(env_metrics, key=lambda x: int(x.split()[0][2:])) 
+env_metrics = sorted(env_metrics, key=lambda x: int(x.split()[0][2:]))
 st.line_chart(df, x="created_at", y=env_metrics)
 
 st.write("**Particle Metrics**")
 particle_metrics = [c for c in df.columns if "particle" in c]
+
 
 def particle_metrics_sorter(pm: str):
     val_str = pm.split()[1][:-2]
@@ -50,7 +83,8 @@ def particle_metrics_sorter(pm: str):
         val_str = "0." + val_str[1:]
     return float(val_str)
 
-particle_metrics = sorted(particle_metrics, key=particle_metrics_sorter) 
+
+particle_metrics = sorted(particle_metrics, key=particle_metrics_sorter)
 st.line_chart(df, x="created_at", y=particle_metrics)
 
 st.divider()
